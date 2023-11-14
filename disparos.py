7cff79a0009ceeca67c7e60cab5e4457be40002a
cammarze <135481707+cammarze@users.noreply.github.com>
2023-11-13 23:31:15 -0300

    Update disparos.py

--- disparos.py
+++ disparos.py
@@ -1,35 +1,38 @@
 import numpy as np
 import pruebo as pr
 
-"""Buenas, aca asumo que la clase Tablero tiene como atributo:
+"""
     self.guardar_disparos = {}
-falta agregar cosas como chequear si ya le pego en la coordenada ingresada pero bueno, lo voy subiendo por si ven algun error (ya que no lo prob;e, o si consideran que falta algo en este codigo)
 """
 
 def disparo(self):
     ubicacion = pr.preguntar_coordenadas("Ingrese las coordenadas: ",
                                          "Coordenadas invalidas\nIngrese nuevamente las coordenadas: ",
-                                         tamano=(self.dim_x, self.dim_y, self.dim_z))
+                                         tamano=(0,0,0))
     
-    guardar_disparos = {} #diccionario para guardar os disparos
+    if ubicacion in self.guardar_disparos:
+        print("Esta coordenada ya fue ingresada anteriormente")
+        return(disparo)
 
     if self.tablero[ubicacion[0], ubicacion[1], ubicacion[2]] == 1:  #cheuqeo si hay un vehiculo en la coordenada ingresada
 
-        vehiculos = [Avion(), Globo(), Zeppelin(), ElevadorEspacial()] #lista para iterar sobre las clases de los vehiculos
-        for vehiculo in vehiculos:
-            vehiculo.RecibirGolpe()
-            if vehiculo.vida == 0:
+        """
+        Algun codigo en donde chequee que la vida es == 0, tengo que consultarle algo a debo y lo hago
+        """
                 #marco que el vehiculo fue derrotado
                 self.tablero[ubicacion[0], ubicacion[1], ubicacion[2]] = 0
-                self.guardar_disparos["Sunk"] = ubicacion #lo guardo en el diccionario
-                return self.guardar_disparos
+                self.guardar_disparos[ubicacion] = "Sunk" 
+                print("Resultado: < Hundido >")
             
         #el vehiculo fue tocado pero no derrotado    
-        self.guardar_disparos["Hit"] = ubicacion
-        return self.guardar_disparos
+        self.guardar_disparos[ubicacion] = "Hit"
+        print("Resultado: < Tocado >")
     else:
         #no se hallo ningun vehiculp en la coordenada ingresada
-        self.guardar_disparos["Miss"] = ubicacion
-        return self.guardar_disparos
+        self.guardar_disparos[ubicacion] = "Miss"
+        print("Resultado: < Errado >")
+        
+    return self.guardar_disparos
+
 
    
