74850fa0f592865b04954bf3d8b0160945e8958d
CatalinaMartinezMartin <141249910+CatalinaMartinezMartin@users.noreply.github.com>
2023-11-23 00:15:08 -0300

    Forma final

--- disparos.py
+++ disparos.py
@@ -1,37 +1,31 @@
 import numpy as np
 import pruebo as pr
 
-import funciones as f
-import clases as c
+import codigo_entero as c
 
-"""
-    self.guardar_vehiculos = {}
-    self.guardar_disparos = {}
-"""
 
-def disparo(self,vehiculo, enemigo):
-    ubicacion = f.preguntar_coordenadas("Ingrese las coordenadas: ",
+def disparo(self, enemigo):
+    ubicacion = c.preguntar_coordenadas("Ingrese las coordenadas: ",
                                         "Coordenadas invalidas\nIngrese nuevamente las coordenadas: ")
     
     while ubicacion in self.guardar_disparos: #Chequeo que el disparo no sea repetido
         print("Esta coordenada ya fue ingresada anteriormente.")
-        ubicacion = f.preguntar_coordenadas("Ingrese las coordenadas nuevamente: ",
+        ubicacion = c.preguntar_coordenadas("Ingrese las coordenadas nuevamente: ",
                                             "Coordenadas invalidas\nIngrese nuevamente las coordenadas: ")
 
     valor_coordenada = enemigo.tablero[ubicacion[0], ubicacion[1], ubicacion[2]] #Coordenada ingresada
 
     if valor_coordenada in enemigo.guardar_vehiculos: #Chequeo si hay algun vehiculo
-        vehiculo.vida -= 1
+        enemigo.guardar_vehiculos[valor_coordenada][0].vida -= 1
 
         #El vehiculo fue tocado    
         self.guardar_disparos[ubicacion] = "Hit"
         self.disp_tablero[ubicacion[0], ubicacion[1], ubicacion[2]] = -2
 
         #El vehículo fue derribado
-        if not vehiculo.vida:
-            
-            
-            enemigo.tablero[ubicacion[0], ubicacion[1], ubicacion[2]] = -3 #Le asigno un valor para que el cambio de colorcito
+        if not  enemigo.guardar_vehiculos[valor_coordenada][0].vida:
+            self.disp_tablero[enemigo.tablero == valor_coordenada] = -3 #Le asigno un valor para el cambio del colorcito
+            enemigo.tablero[enemigo.tablero == valor_coordenada] = -3
             self.guardar_disparos[ubicacion] = "Sunk"
             print("Resultado: < Hundido >")
         else:
