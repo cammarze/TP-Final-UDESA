1af3b9b778a61543f59256f17bcac69aa84373db
cammarze <135481707+cammarze@users.noreply.github.com>
2023-11-24 17:07:31 -0300

    imagimag miah

--- mostrarimagenes.py
+++ mostrarimagenes.py
@@ -17,16 +17,20 @@ fig, axs = plt.subplots(2, 2) #crea cuadricula 2x2
 
 #muestra las imágenes en cada uno de los subplots
 axs[0, 0].imshow(image1) #fila 0, col 0, primer eje
-axs[0, 0].set_title('Imagen 1')
+axs[0, 0].set_title('Globo',fontsize=12, color='black')
+axs[0, 0].set_axis_off()
 
 axs[0, 1].imshow(image2)
-axs[0, 1].set_title('Imagen 2')
+axs[0, 1].set_title('Avion',fontsize=12, color='black')
+axs[0, 1].set_axis_off()
 
 axs[1, 0].imshow(image3) 
-axs[1, 0].set_title('Imagen 3')
+axs[1, 0].set_title('Elevador Espacial',fontsize=12, color='black')
+axs[1, 0].set_axis_off() #saca los ejes
 
 axs[1, 1].imshow(image4)
-axs[1, 1].set_title('Imagen 4')
+axs[1, 1].set_title('Zeppelin',fontsize=12, color='black')
+axs[1, 1].set_axis_off()
 
 plt.tight_layout()
 
