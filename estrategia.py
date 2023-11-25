bcfcd7eeac33d22a25af7f3bd00d63543befec61
CatalinaMartinezMartin <141249910+CatalinaMartinezMartin@users.noreply.github.com>
2023-11-25 20:41:59 -0300

    Estrategia optimizada
    Agregue 2 funciones auxiliares

--- estrategia.py
+++ estrategia.py
@@ -1,4 +1,25 @@
 import codigo_entero as c
+import random
+
+def check_borde(x, y, z, diff = 0):
+    #Llego al borde del tablero, recorro la misma diagonal pero mas arriba
+    if y < 0:
+        x = 14
+        y = x - diff
+        z += 2
+        #Me muevo a la siguiente diagonal
+        if z > 9:
+            x = 14
+            y -= 2
+            z = 0
+    return x, y, z
+
+def mov_diagonal(x, y, z, dx, dy, diff = 0):
+    #Mientras mas grandes san dx y dy, mas distanciados estaran los disparos
+    x -= dx
+    y -= dy
+    x, y, z = check_borde(x, y, z, diff)
+    return x, y, z
 
 def next_turn(coor_compu):
     coor_compu = coor_compu.split()
@@ -6,210 +27,42 @@ def next_turn(coor_compu):
     compu_y = int(coor_compu[1])
     compu_z = int(coor_compu[2])
 
-    if compu_x == compu_y:
-        compu_x = 16
-        compu_y = 14
-    
     #Primera mitad
     if compu_x > compu_y:
-        if (compu_x - compu_y) == 2: #Diagonal
-            compu_x -= 2
-            compu_y -= 2
-            if compu_y < 0: #Llego al borde del tablero
-                compu_x = 16
+        diff = compu_x - compu_y
+        if diff in [2, 4, 6, 8, 10, 12]:
+            compu_x, compu_y, compu_z = mov_diagonal(compu_x, compu_y, compu_z, 2, 2, diff)
+
+        elif diff == 14:
+            compu_z += 2
+            if compu_z > 9:
+                compu_x = 14
+                compu_y = 16
+                compu_z = 0
+            else:
+                compu_x, compu_y, compu_z = check_borde(compu_x, compu_y, compu_z, diff)
+
+    #Segunda mitad
+    elif compu_x < compu_y:
+        diff = compu_y - compu_x
+        if diff in [2, 4, 6, 8, 10, 12]:
+            compu_y, compu_x, compu_z = mov_diagonal(compu_y, compu_x, compu_z, 2, 2, diff)
+
+        elif diff == 14:
+            compu_z += 2
+            if compu_z > 9:
+                compu_x = 14
                 compu_y = 14
-                compu_z += 2
-                if compu_z == 10: #Paso a la siguiente diagonal
-                    compu_x = 16
-                    compu_y = 12
-                    compu_z = 0
+                compu_z = 0
             else:
-                coor_compu = f"{compu_x} {compu_y} {compu_z}"
-                return coor_compu
+                compu_y, compu_x, compu_z = check_borde(compu_y, compu_x, compu_z, diff)
 
-        if (compu_x - compu_y) == 4: #Diagonal
-            compu_x -= 2
-            compu_y -= 2
-            if compu_y < 0:
-                compu_x = 16
-                compu_y = 12
-                compu_z += 2
-                if compu_z == 10:
-                    compu_x = 16
-                    compu_y = 10
-                    compu_z = 0
-            else:
-                coor_compu = f"{compu_x} {compu_y} {compu_z}"
-                return coor_compu
-            
-        if (compu_x - compu_y) == 6: #Diagonal
-            compu_x -= 2
-            compu_y -= 2
-            if compu_y < 0:
-                compu_x = 16
-                compu_y = 10
-                compu_z += 2
-                if compu_z == 10:
-                    compu_x = 16
-                    compu_y = 8
-                    compu_z = 0
-            else:
-                coor_compu = f"{compu_x} {compu_y} {compu_z}"
-                return coor_compu
-            
-        if (compu_x - compu_y) == 8: #Diagonal
-            compu_x -= 2
-            compu_y -= 2
-            if compu_y < 0:
-                compu_x = 16
-                compu_y = 8
-                compu_z += 2
-                if compu_z == 10:
-                    compu_x = 16
-                    compu_y = 6
-                    compu_z = 0
-            else:
-                coor_compu = f"{compu_x} {compu_y} {compu_z}"
-                return coor_compu
-            
-        if (compu_x - compu_y) == 10: #Diagonal
-            compu_x -= 2
-            compu_y -= 2
-            if compu_y < 0:
-                compu_x = 16
-                compu_y = 6
-                compu_z += 2
-                if compu_z == 10:
-                    compu_x = 16
-                    compu_y = 4
-                    compu_z = 0
-            else:
-                coor_compu = f"{compu_x} {compu_y} {compu_z}"
-                return coor_compu
-            
-        if (compu_x - compu_y) == 12: #Diagonal
-            compu_x -= 2
-            compu_y -= 2
-            if compu_y < 0:
-                compu_x = 16
-                compu_y = 4
-                compu_z += 2
-                if compu_z == 10:
-                    compu_x = 14
-                    compu_y = 0
-                    compu_z = 10
-            else:
-                coor_compu = f"{compu_x} {compu_y} {compu_z}"
-                return coor_compu
-                
-        #Esquina
-        if (compu_x - compu_y) == 14:
-            compu_z -= 2
-            if compu_z < 0: #Comienzo a recorrer otra mitad
-                compu_x = 14
-                compu_y = 16
-            else:
-                coor_compu = f"{compu_x} {compu_y} {compu_z}"
-                return coor_compu
+    #Diagonal principal
+    elif compu_x == compu_y:
+        compu_x, compu_y, compu_z = mov_diagonal(compu_x, compu_y, compu_z, 2, 2)
+        if compu_z > 9:
+            compu_x = 14
+            compu_y = 12
+            compu_z = 0
 
-
-    #Segunda mitad           
-    if compu_x < compu_y:
-        if (compu_y - compu_x) == 2: #Diagonal
-            compu_x -= 2
-            compu_y -= 2
-            if compu_x < 0: #Llego al borde del tablero
-                compu_x = 14
-                compu_y = 16
-                compu_z += 2
-                if compu_z == 10: #Paso a la siguiente diagonal
-                    compu_x = 12
-                    compu_y = 16
-                    compu_z = 0
-            else:
-                coor_compu = f"{compu_x} {compu_y} {compu_z}"
-                return coor_compu
-
-        if (compu_y - compu_x) == 4: #Diagonal
-            compu_x -= 2
-            compu_y -= 2
-            if compu_x < 0:
-                compu_x = 12
-                compu_y = 16
-                compu_z += 2
-                if compu_z == 10:
-                    compu_x = 10
-                    compu_y = 16
-                    compu_z = 0
-            else:
-                coor_compu = f"{compu_x} {compu_y} {compu_z}"
-                return coor_compu
-            
-        if (compu_y - compu_x) == 6: #Diagonal
-            compu_x -= 2
-            compu_y -= 2
-            if compu_x < 0:
-                compu_x = 10
-                compu_y = 16
-                compu_z += 2
-                if compu_z == 10:
-                    compu_x = 8
-                    compu_y = 16
-                    compu_z = 0
-            else:
-                coor_compu = f"{compu_x} {compu_y} {compu_z}"
-                return coor_compu
-            
-        if (compu_y - compu_x) == 8: #Diagonal
-            compu_x -= 2
-            compu_y -= 2
-            if compu_x < 0:
-                compu_x = 8
-                compu_y = 16
-                compu_z += 2
-                if compu_z == 10:
-                    compu_x = 6
-                    compu_y = 16
-                    compu_z = 0
-            else:
-                coor_compu = f"{compu_x} {compu_y} {compu_z}"
-                return coor_compu
-            
-        if (compu_y - compu_x) == 10:
-            compu_x -= 2
-            compu_y -= 2
-            if compu_x < 0:
-                compu_x = 6
-                compu_y = 16
-                compu_z += 2
-                if compu_z == 10:
-                    compu_x = 4
-                    compu_y = 16
-                    compu_z = 0
-            else:
-                coor_compu = f"{compu_x} {compu_y} {compu_z}"
-                return coor_compu
-            
-        if (compu_y - compu_x) == 12:
-            compu_x -= 2
-            compu_y -= 2
-            if compu_x < 0:
-                compu_x = 4
-                compu_y = 16
-                compu_z += 2
-                if compu_z == 10:
-                    compu_x = 0
-                    compu_y = 14
-                    compu_z = 10
-            else:
-                coor_compu = f"{compu_x} {compu_y} {compu_z}"
-                return coor_compu
-            
-        #Esquina
-        if (compu_y - compu_x) == 14:
-            compu_z -= 2
-            if compu_z < 0: #Comienzo a recorrer otra mitad
-                pass
-            else:
-                coor_compu = f"{compu_x} {compu_y} {compu_z}"
-                return coor_compu
+    return f"{compu_x} {compu_y} {compu_z}"
