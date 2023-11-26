b7d4e7ab081a099c655d3ff609434cc3bbd815e7
LucaDeNotta <141259976+LucaDeNotta@users.noreply.github.com>
2023-11-26 02:58:19 -0300

    Dice Cata Y Luca Y Cami que odian al que puso los aviones en el juego

--- estrategia.py
+++ estrategia.py
@@ -18,22 +18,30 @@ def check_borde(x: int, y: int, z: int, diff: int = 0) -> tuple :
         z (int) --> Valor (actualizado) del disparo en la coordenada z
 
     """
-    #Llego al borde del tablero, recorro la misma diagonal pero mas arriba
-    if y < 0:
+    if y < 0 and not(x % 2):
         x = 14
         y = x - diff
         z += 2
-        #Me muevo a la siguiente diagonal
-        if z > 9:
+        # Me muevo a la siguiente diagonal
+        if z > 9 and not(x % 2):
             x = 14
-            y -= 1
+            y -= 2
+            z = 0
+    elif y < 0 and (x % 2):
+        x = 13
+        y = x - diff
+        if not z:
+            z += 1
+        else:
+            z += 2
+        if z > 9 and (x % 2):
+            x = 13
+            y -= 2
             z = 0
     return x, y, z
 
-def mov_diagonal(x: int, y, z, dx, dy, diff = 0):
-    """
-    """
-    #Mientras mas grandes san dx y dy, mas distanciados estaran los disparos
+def mov_diagonal(x, y, z, dx, dy, diff = 0):
+    #Mientras mas grandes sean dx y dy, mas distanciados estaran los disparos
     x -= dx
     y -= dy
     x, y, z = check_borde(x, y, z, diff)
@@ -45,11 +53,11 @@ def next_turn(coor_compu):
     compu_y = int(coor_compu[1])
     compu_z = int(coor_compu[2])
 
-    #Primera mitad
-    if compu_x > compu_y:
+    #Primera mitad diagonal pares
+    if compu_x > compu_y and not(compu_x % 2):
         diff = compu_x - compu_y
-        if diff in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]:
-            compu_x, compu_y, compu_z = mov_diagonal(compu_x, compu_y, compu_z, 2, 1, diff)
+        if diff in [2, 4, 6, 8, 10, 12]:
+            compu_x, compu_y, compu_z = mov_diagonal(compu_x, compu_y, compu_z, 2, 2, diff)
 
         elif diff == 14:
             compu_z += 2
@@ -60,11 +68,11 @@ def next_turn(coor_compu):
             else:
                 compu_x, compu_y, compu_z = check_borde(compu_x, compu_y, compu_z, diff)
 
-    #Segunda mitad
-    elif compu_x < compu_y:
+    #Segunda mitad pares
+    elif compu_x < compu_y and not(compu_x % 2):
         diff = compu_y - compu_x
-        if diff in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]:
-            compu_y, compu_x, compu_z = mov_diagonal(compu_y, compu_x, compu_z, 2, 1, diff)
+        if diff in [2, 4, 6, 8, 10, 12]:
+            compu_y, compu_x, compu_z = mov_diagonal(compu_y, compu_x, compu_z, 2, 2, diff)
 
         elif diff == 14:
             compu_z += 2
@@ -75,12 +83,44 @@ def next_turn(coor_compu):
             else:
                 compu_y, compu_x, compu_z = check_borde(compu_y, compu_x, compu_z, diff)
 
-    #Diagonal principal
+    #Diagonal principal pares e impares
     elif compu_x == compu_y:
-        compu_x, compu_y, compu_z = mov_diagonal(compu_x, compu_y, compu_z, 2, 1)
-        if compu_z > 9:
-            compu_x = 14
-            compu_y = 12
+        if not(compu_x == 0 and compu_y == 0 and compu_z == 8):
+            compu_x, compu_y, compu_z = mov_diagonal(compu_x, compu_y, compu_z, 2, 2)
+        else:
+            compu_x = 13
+            compu_y = 11
             compu_z = 0
+
+    #Primera mitad diagonal impares
+    elif compu_x > compu_y and (compu_x % 2):
+        diff = compu_x - compu_y
+        if diff in [2, 4, 6, 8, 10]:
+            compu_x, compu_y, compu_z = mov_diagonal(compu_x, compu_y, compu_z, 2, 2, diff)
+
+        elif diff == 12:
+            compu_z += 3
+            if compu_z > 9:
+                compu_x = 11
+                compu_y = 13
+                compu_z = 0
+            else:
+                compu_x, compu_y, compu_z = check_borde(compu_x, compu_y, compu_z, diff)
+
+    #Segunda mitad impares
+    elif compu_x < compu_y and (compu_x % 2):
+        print("Segunda mitad")
+        diff = compu_y - compu_x
+        if diff in [2, 4, 6, 8, 10]:
+            compu_y, compu_x, compu_z = mov_diagonal(compu_y, compu_x, compu_z, 2, 2, diff)
+
+        elif diff == 12:
+            compu_z += 3
+            if compu_z > 9:
+                compu_x = 13
+                compu_y = 13
+                compu_z = 0
+            else:
+                compu_y, compu_x, compu_z = check_borde(compu_y, compu_x, compu_z, diff)
 
     return f"{compu_x} {compu_y} {compu_z}"
