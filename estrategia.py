f32dfc62df3e096bb230088ddcf5cfe81f72cb1c
LucaDeNotta <141259976+LucaDeNotta@users.noreply.github.com>
2023-11-25 21:40:05 -0300

    Docstring Cata

--- estrategia.py
+++ estrategia.py
@@ -1,11 +1,22 @@
-import codigo_entero as c
-import random
-
-def check_borde(x, y, z, diff = 0):
+def check_borde(x: int, y: int, z: int, diff: int = 0) -> tuple :
     """
-    Chequea si los valores de (x, y, z) se encuentran dentro de los limites tablero. Si se encuentran fuera,
-    cambia sus valores para
-    1) Resetea los valores
+    Chequea si los valores de (x, y, z) estan dentro de los limites del tablero. Si estan fuera,
+    los ajusta de la siguiente manera:
+    1) Reinicia los valores de (x, y) a los de la diagonal correspondiente. Cambia el valor de z para
+    moverse a la misma diagonal pero "mas arriba".
+    2) Cambia los valores de (x, y, z) para cambiar a la siguiente diagonal.
+
+    Args:
+        x (int): Valor del disparo en la coordenada x
+        y (int): Valor del disparo en la coordenada y
+        z (int): Valor del disparo en la coordenada z
+        diff (int): Diferencia entre los valores de x e y
+
+    Returns:
+        x (int) --> Valor (actualizado) del disparo en la coordenada x
+        y (int) --> Valor (actualizado) del disparo en la coordenada y
+        z (int) --> Valor (actualizado) del disparo en la coordenada z
+
     """
     #Llego al borde del tablero, recorro la misma diagonal pero mas arriba
     if y < 0:
@@ -19,7 +30,9 @@ def check_borde(x, y, z, diff = 0):
             z = 0
     return x, y, z
 
-def mov_diagonal(x, y, z, dx, dy, diff = 0):
+def mov_diagonal(x: int, y, z, dx, dy, diff = 0):
+    """
+    """
     #Mientras mas grandes san dx y dy, mas distanciados estaran los disparos
     x -= dx
     y -= dy
@@ -41,8 +54,8 @@ def next_turn(coor_compu):
         elif diff == 14:
             compu_z += 2
             if compu_z > 9:
-                compu_x = 14
-                compu_y = 16
+                compu_x = 12
+                compu_y = 14
                 compu_z = 0
             else:
                 compu_x, compu_y, compu_z = check_borde(compu_x, compu_y, compu_z, diff)
